14a475b77635bed0d8c0a50762cff0d49d5eb99b
chenJunYan <2562178620@qq.com>
2022-12-12 00:43:01 +0800

    优化readMe； 添加伴侣的输入框

--- src/pages/Companion/AddCompanion.jsx
+++ src/pages/Companion/AddCompanion.jsx
@@ -1,11 +1,31 @@
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const AddCompanion = () => {
+  const [email, setEmail] = React.useState("");
+
   return (
     <div>
-      添加伴侣
+      <Box>
+        {/* 邮箱 */}
+        <TextField
+          // id="outlined-name"
+          label="输入伴侣邮箱 Email"
+          autoFocus={true}
+          error={false} //输入错误
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+            // console.log(event.target.value);
+          }}
+          onBlur={() => {
+            // alert("email");
+          }}
+        />
+      </Box>
     </div>
   );
 };
 
 export default AddCompanion;
-
