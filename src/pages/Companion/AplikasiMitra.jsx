d196a5630d7505712cb979ba128d4248b2c3b9d5
JunYan101 <2562178620@qq.com>
2022-12-29 14:47:49 +0800

    可以显示伴侣的申请列表

--- src/pages/Companion/AplikasiMitra.jsx
+++ src/pages/Companion/AplikasiMitra.jsx
@@ -6,13 +6,21 @@ import { useSelector, useDispatch } from "react-redux";
 export default function AplikasiMitra() {
   const StateEmail = useSelector((state) => state.UserName);
 
+  const [Isapplication, SetIsapplication] = React.useState(false);
+  const [applicationListDATA, SetapplicationListDATA] = React.useState();
+
   useEffect(() => {
     ApplicationList(StateEmail);
   }, []);
 
-  const [Isapplication, SetIsapplication] = React.useState(false);
-  const [applicationListDATA, SetapplicationListDATA] = React.useState([]);
-  async function ApplicationList(StateEmail) {
+  useEffect(() => {
+    if (applicationListDATA != null) {
+      SetIsapplication(true);
+    }
+  }, [applicationListDATA]);
+
+  const ApplicationList = async (StateEmail) => {
+    // console.log(typeof(StateEmail));
     await fetch("api/WievUserApplication?email=" + StateEmail, {
       method: "GET",
       headers: {
@@ -28,34 +36,32 @@ export default function AplikasiMitra() {
       })
       .then((data) => {
         SetapplicationListDATA(data);
-        console.log(applicationListDATA);
-        SetIsapplication(true);
       });
-  }
+  };
 
   return (
     <>
       <OverallCentered>
         <Box sx={{ textAlign: "center", mb: 3 }}>查找伴侣</Box>
-        {/* {Isapplication ? <ApplicationMap list={applicationListDATA} /> : null} */}
+        {Isapplication ? (
+          <ApplicationMap listDATA={applicationListDATA} />
+        ) : null}
       </OverallCentered>
     </>
   );
 }
 
-const ApplicationMap = ({ listDATA }) => {
-  console.log(listDATA[0]);
+const ApplicationMap = (props) => {
+  const { listDATA } = props;
   return (
     <div>
       {listDATA.map((item, index) => {
-        console.log(item.applicant_id);
-        console.log(item.user_id);
-        // return (
-        //   <Box key={index}>
-        //     <div>{item.applicant_id}</div>
-        //     <div>{item.user_id}</div>
-        //   </Box>
-        // );
+        return (
+          <Box key={index}>
+            <div>{item["applicant_id"]}</div>
+            <div>{item["user_id"]}</div>
+          </Box>
+        );
       })}
     </div>
   );
